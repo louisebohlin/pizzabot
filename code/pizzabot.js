d64677f2e69e0c2bcac3b6da87fe8a1a6182c220
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-09-21 13:43:15 +0200

    Final version with comments

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,14 +5,14 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 
-
+//welcome message, an alert with a string
 alert("Hey! Happy to serve your pizza. On our menu we have Vegetarian Pizza, Hawaiian Pizza and Pepperoni Pizza")
 
-const pizzaType = [vegetarian, hawaiian, pepperoni]
-
+//variable which gives us an ordername
 const orderName = prompt("Enter the name of the pizza you want to order today.")
 
-const checkPizza = (theOrderName) => {
+//function which compares input with pizza variables
+const checkOrderName = (theOrderName) => {
   if (theOrderName != vegetarian && theOrderName != hawaiian && theOrderName != pepperoni) {
     alert("Sorry, we don't have that pizza on our menu.")
     return false
@@ -21,18 +21,20 @@ const checkPizza = (theOrderName) => {
   }
 }
 
-const res = checkPizza(orderName) // res = true eller false
+//invokes the function if true or false
+const res = checkOrderName(orderName) // res = true eller false
 
-
+//the rest will proceed if result is true
 if(res){
+  //function gives input on number of pizzas ordered
   const orderQuantity = prompt("How many of " + orderName +" do you want?")
-
-  const calculateOrderTotal = () => {
+  //function which calculates the total price
+  const totalCost = () => {
     return orderQuantity * pizzaPrice
   }
-
-  const orderTotal = calculateOrderTotal()
-
+  //invokes the function
+  const orderTotal = totalCost()
+  //function that calculates the cooking time
   const calculateCookingTime = () => {
     if (orderQuantity <= 2) {
       return (10)
@@ -42,9 +44,9 @@ if(res){
       return (15)
   }
 }
-
+  //invokes the function
   const cookingTime = calculateCookingTime()
-
+//final alert to customer
 alert("Great, I'll get started on your " + orderName + " right away. It will cost " + orderTotal + " kr. The pizzas will take " + cookingTime + " min.")
 
 }
