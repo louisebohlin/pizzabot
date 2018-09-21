77ae1c48baf07504998fc52fe93b2c337410b836
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-09-21 11:43:07 +0200

    First basics

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,4 +4,20 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-//Put your Javscript code here:
+
+
+alert("Hey! Happy to serve your pizza. On our menu we have Vegetarian Pizza, Hawaiian Pizza and Pepperoni Pizza")
+
+const pizzaType = [vegetarian, hawaiian, pepperoni]
+
+const orderName = prompt("Enter the name of the pizza you want to order today.")
+
+const orderQuantity = prompt("How many of " + orderName +" do you want?")
+
+const calculateOrderTotal = () => {
+  return orderQuantity * pizzaPrice
+}
+
+const orderTotal = calculateOrderTotal()
+
+alert("Great, I'll get started on your " + orderName + " right away, it will cost " + orderTotal + " kr")
